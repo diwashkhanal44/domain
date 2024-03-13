363aec0feabd61d66f0f30b03b2d897b616f30f2
diwashkhanal44 <diwashkhanal44@gmail.com>
2024-03-13 14:00:33 +1100

    withjs

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 <html>
-    <head>
-        <body>
-            <h1>portfolio from js</h1>
-        </body>
-    </head>
+  <head>
+    <title>Portfolio from JavaScript</title> </head>
+  <body>
+    <h1 id="h1">Portfolio from JS</h1>
+    <button onclick="change()">Change Text</button> <script>
+      function change() {
+        document.getElementById("h1").innerText = "hahaha";
+      }
+    </script>
+  </body>
 </html>

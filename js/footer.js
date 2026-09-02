18456bdeca5334eab2d2a445bf9adfb2b0218b89
diwashkhanal44 <diwashkhanal44@gmail.com>
2026-09-02 15:45:45 +1000

    changes

--- js/footer.js
+++
@@ -1,5 +0,0 @@
-const footerContainer = document.getElementById("footer-container");
-fetch("./footer.html")
-    .then(function (response) { return response.text(); })
-    .then(function (html) { footerContainer.innerHTML = html; })
-    .catch(function (error) { console.error("Error fetching footer:", error); });
